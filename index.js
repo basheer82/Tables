153d681485ed827a4f77a273c9ec1b419862c48b
basheer82 <73847889+basheer82@users.noreply.github.com>
2020-11-02 23:52:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var principal = 1000;
 var rate = 0.05;
 var year;
 var rowsClasses = [" ","table-info","table-primary", "table-light", "table-success", "table-danger", "table-warning", "table-info", "table-primary", "table-success", "table-info" ];
-document.writeln("<p><h2>Annual Deposit of 1000 and final year Amount at different Compound intrest rates</h2></p>");
+document.writeln("<p><h2>Annual Deposit of 1000 and final year amount at different Compound Intrest Rates</h2></p>");
+document.writeln("<p><h2>using Javascript and Bootstrap</h2></p>");
 for ( var counter = 1; counter <= 5; counter++)
 {
   document.writeln("<div class='container-fluid col-9'>");
@@ -13,8 +14,8 @@ for ( var counter = 1; counter <= 5; counter++)
   document.writeln("<thead><tr><th>Year</th><th>Amount</th></tr></thead><tbody>");
     for ( year = 1; year <= 10; year++)
     {
-     amount = principal * Math.pow( 1.0 + rate, year);
-    document.writeln("<tr class = " + rowsClasses[year] + "><td>" + year + "</td><td>" + amount.toFixed(2) + "</td></tr>");
+      amount = principal * Math.pow( 1.0 + rate, year);
+      document.writeln("<tr class = " + rowsClasses[year] + "><td>" + year + "</td><td>" + amount.toFixed(2) + "</td></tr>");
     }
   document.writeln("</tbody></table>");
   document.writeln("</div>");
